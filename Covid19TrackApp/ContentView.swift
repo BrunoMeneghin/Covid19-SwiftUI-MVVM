c76e0cf0ef0fc5e8fbbdcde37b68104da2d1b41b
Bruno Meneghin <bruno.meneghin@meifacil.com>
2020-06-01 14:38:09 -0300

    Initial Commit

---
+++ Covid19TrackApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19TrackApp
+//
+//  Created by Bruno Meneghin on 01/06/20.
+//  Copyright © 2020 Bruno Meneghin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
